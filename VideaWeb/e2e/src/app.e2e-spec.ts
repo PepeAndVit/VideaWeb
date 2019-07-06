5b299fa6c3088e00eb90c59887d4200238508456
PepeAndVit <49903460+PepeAndVit@users.noreply.github.com>
2019-07-06 18:25:10 +0200

    Proyecto sin enrutamiento

--- VideaWeb/e2e/src/app.e2e-spec.ts
+++ VideaWeb/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to VideaWeb!');
+    expect(page.getTitleText()).toEqual('Welcome to Publicolor!');
   });
 
   afterEach(async () => {
